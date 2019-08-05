5d17e8e219c222d83c4086835f26857950da59c7
Hoang Cao <hoangcao@mail.weber.edu>
2019-08-04 20:35:26 -0700

    add contact page

--- assets/js/wcs.js
+++ assets/js/wcs.js
@@ -22,28 +22,29 @@ function openNav() {
     document.getElementById('number').value = value;
   }
 
-  document.querySelector(".next").addEventListener("click", showStep2);
-  function showStep2()
-  {
-    document.querySelector(".hidden2").style.display = "block";
-  }
+  // document.querySelector(".next").addEventListener("click", showStep2);
+  
 
-  function showStep3()
-  {
-    document.querySelector(".hidden3").style.display = "none";
-  }
+  // function showStep3()
+  // {
+  //   document.querySelector(".hidden3").style.display = "none";
+  // }
 $("#bootstrapForm").submit(function(event) {
 
   // make selected form variable
-  var vForm = $(this);
-  
+  var vForm = $(this); 
   if (vForm[0].checkValidity() === false) {
     event.preventDefault()
     event.stopPropagation()
   } else {
-   
-    alert("your form is valid and ready to send");
-    
+    event.preventDefault()
+    document.querySelector(".next").addEventListener("click", foo());
+    function foo()
+    {
+      document.querySelector(".hidden2").style.display = "block";
+      document.querySelector(".next").disabled = true;
+    }
+  
   }
   
   vForm.addClass('was-validated');
