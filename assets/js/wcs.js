c74c9cf46a17d27094e07ee847faf6c5bd1633d0
Hoang Cao <hoangcao@mail.weber.edu>
2019-08-08 15:19:33 -0700

    fixed bugs

--- assets/js/wcs.js
+++ assets/js/wcs.js
@@ -15,38 +15,51 @@
 
 
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
-  
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
+  document.getElementById("myNav").style.width = "100%";
+}
 
-  // document.querySelector(".next").addEventListener("click", showStep2);
-  
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
 
-  // function showStep3()
-  // {
-  //   document.querySelector(".hidden3").style.display = "none";
-  // }
-$("#bootstrapForm").submit(function(event) {
+document.querySelector(".reset").addEventListener("click", reset1);
+function reset1()
+{
+  document.querySelector("#bootstrapForm").reset();
+}
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+// document.querySelector(".next").addEventListener("click", showStep2);
+
+// function showStep2()
+// {
+//   document.querySelector(".hidden2").style.display = "none";
+// }
+
+// function showStep3()
+// {
+//   document.querySelector(".hidden3").style.display = "none";
+// }
+$("#bootstrapForm").submit(function (event) {
   event.preventDefault();
   // make selected form variable
-  var vForm = $(this); 
+  var vForm = $(this);
   if (vForm[0].checkValidity() === false) {
     event.preventDefault()
     event.stopPropagation()
   } else {
     event.preventDefault()
     document.querySelector(".next").addEventListener("click", foo());
-    function foo()
-    {
+    function foo() {
       document.querySelector(".hidden2").style.display = "block";
       document.querySelector(".next").disabled = true;
     }
-  
+
   }
-  
+
   vForm.addClass('was-validated');
-  
+
 });
